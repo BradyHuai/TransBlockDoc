72455cdef7079139a876b4811a219b58aaf617ec
brady <bradyhuai@outlook.com>
2020-12-19 16:51:21 -0800

    Add method docs

---
+++ docs/blockInstance.py
@@ -0,0 +1,115 @@
+
+def addElement(element):
+    """
+    Add element to the elements list. Only valid DOM objects will be displayed in the collection.\n
+    :param element: The DOM element to be added
+    :param type: DOM element
+    :return: null
+    """
+
+def setSize(w, h):
+    """
+    Resets the size of the block container.\n
+    :param w:  Width to be added
+    :param h:  Height to be added
+    :param type: integer
+    :return: null
+    """
+
+def removeElement(i):
+    """
+    Removes the element from the elements list.\n
+    :param i: index of the element to be removed
+    :param type: integer
+    :return: null
+    """
+
+def setTiming(time):
+    """
+    Reset teh default timing of the display time of the elements.\n
+    :param time:  Set the display time of the elements
+    :param type: integer
+    :return: null
+    """
+    return "documentation"
+
+def setButton(previous, next):
+    """
+    Reset the default icons of the buttons, the defaults are simply the left right up down arrows.\n
+    Butons can be customized.\n
+    :param previous:  Set the previousButton
+    :param next:  Set the nextButton
+    :param type: BOM element
+    :return: null
+    """
+    return "documentation"
+
+def generateButtons():
+    """
+    Generates the previous and next buttons\n
+    :return: null
+    """
+    return "documentation"
+
+def disableBlur():
+    """
+    When block is hovered over, the block will be blured, this method disables the effect.\n
+    :return: null
+    """
+
+def setBlurContent(text):
+    """
+    When block is hovered over, this message will be displayed.\n
+    :param text: Text to be set as the display message
+    :param type: string
+    :return: null
+    """
+
+def activateNoAnimation():
+    """
+    Activates no animation for the element transition.\n
+    :return: null
+    """
+
+def activateRightSwipe():
+    """
+    Activates right swipe animation for the element transition.\n
+    The previous element is swiped to the left.\n
+    :return: null
+    """
+
+def activateUpSwipe():
+    """
+    Activates up swipe animation for the element transition.\n
+    The previous element is swiped downwards.\n
+    :return: null
+    """
+
+def activateCircle():
+    """
+    Activates enlarging circle animation for the element transition.\n
+    The previous element is getting smaller as a circle.\n
+    The next element is enlarged as a circle.\n
+    :return: null
+    """
+
+def activateFade():
+    """
+    Activates fade animation for the element transition\n
+    The previous element is faded out while the next element is faded in.\n
+    :return: null
+    """
+
+def activateXSlice():
+    """
+    Activates vertical slice animation for the element transition\n
+    The width is being divided into 4 parts and each part is slowly being replaced by the next element.\n
+    :return: null
+    """
+
+def activateYSlice():
+    """
+    Activates horizontal slice animation for the element transition\n
+    The height is being divided into 4 parts and each part is slowly being replaced by the next element.\n
+    :return: null
+    """
--- docs/index.rst
+++ docs/index.rst
@@ -6,27 +6,78 @@
 Welcome to TransBlock's documentation!
 ======================================
 
-=======
-Create TransBlock Instance
-======
+===============
+Getting Started
+===============
+
+
+===========
+Constructor
+===========
 
 To create a TransBlock Instance, use the new BlockGenerator() function
 
 >> const blockInstance = new BlockGenerator();
 
-.. list-table:: Title
-   :widths: 25 25 50
+==========
+Properties
+==========
+
+.. list-table:: Options
+   :widths: 20 10 20 50
    :header-rows: 1
 
-   * - Heading row 1, column 1
-     - Heading row 1, column 2
-     - Heading row 1, column 3
-   * - Row 1, column 1
-     -
-     - Row 1, column 3
-   * - Row 2, column 1
-     - Row 2, column 2
-     - Row 2, column 3
+   * - Property
+     - Default
+     - Type
+     - Description
+   * - block
+     - 'div'
+     - DOM element
+     - The container for the entire block.
+   * - itemList
+     - 'div'
+     - DOM element
+     - The list containing all elements inside the collection.
+   * - buttonPrevious
+     - 'i'
+     - DOM element
+     - The button that can be clicked to move back to the previous element in the collection.
+   * - buttonNext
+     - 'i'
+     - DOM element
+     - The button that can be clicked to move back to the next element in the collection.
+   * - width
+     - 800
+     - integer
+     - The width of the block container
+   * - height
+     - 500
+     - integer
+     - The height of the block container
+   * - timing
+     - 4
+     - integer
+     - The display time for each element in the collection when the itemList is being iterated.
+   * - counter
+     - \
+     - integer
+     - The index of the current element that is on display
+   * - elements
+     - []
+     - Array
+     - The list of elements add to the container
+   * - hovered_flag
+     - false
+     - boolean
+     - The status whether the block object is being hovered over.
+
+=======
+Methods
+=======
+
+.. automodule:: blockInstance
+  :members:
 
 
 .. toctree::
